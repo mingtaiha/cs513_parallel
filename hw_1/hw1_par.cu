da915cb11e0eeb9948c299ed309526a33a03d8b0
Ming Ha <mingtha@utility.cs.rutgers.edu>
2017-02-19 23:07:46 -0500

    Fixes

--- hw_1/hw1_par.cu
+++ hw_1/hw1_par.cu
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <ctime>
 #include <cstdlib>
 #include <cmath>
@@ -8,67 +9,38 @@
 using namespace std;
 
 #define TPB 1024
-#define RANGE 10
+#define RANGE 100
 
 #define min(a,b) ((a < b) ? a : b)
 
-__global__
-void incl_pfsum (float * array, int size) {
 
-	int bsize = blockIdx.x * blockDim.x;
-	int tid = bsize + threadIdx.x;
-	int tmp;
-
-	size = min(size, TPB);
-
-	__syncthreads();
-
-	
-	for (int step = 2; step <= size ; step *= 2) {
-		if (tid % step == (step - 1) && (tid - (step / 2) >= bsize)) {
-			array[tid] += array[tid - (step / 2)];
-		}
-		__syncthreads();
-	}
-	__syncthreads();
-
-	if (tid % TPB == 0) { array[bsize - 1] = 0; } 
-	__syncthreads();
-
-	for (int step = size; step > 0; step /= 2) {
-		if (tid % step == (step - 1) && (tid - (step / 2) >= bsize)) {
-			tmp = array[tid];
-			//__syncthreads();
-			array[tid] += array[tid - (step / 2)];
-			//__syncthreads();
-			array[tid - (step / 2)] = tmp;
-			//__syncthreads();
-		}
-		__syncthreads();
-	}
-	__syncthreads();
-}
 
 __global__
-void scat_part_sum(float * array, float * array_psums) {
+void scat_part_sum(double * array, double * array_psums) {
 
+	// Distributes the values from array_psums (array of partial sums) to every element
+	// in the array. Every thread in a block gets the same partial sum added to it
 	int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
-	__syncthreads();
+	//__syncthreads();
 
 	array[tid] += array_psums[blockIdx.x];
-	__syncthreads();
+	//__syncthreads();
 }
 
 __global__
-void upsweep (float * array, float * array_aggr1, int size, int size_aggr1) {
+void upsweep (double * array, double * array_aggr1, int size) {
+
+	// Performs an upsweep
 
 	int bid = blockIdx.x * blockDim.x;
 	int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
 
+	int min_size = min(size, TPB);
 	__syncthreads();
 
+	// Merge elements like a binary tree
 	
-	for (int step = 2; step <= size ; step *= 2) {
+	for (int step = 2; step <= min_size ; step *= 2) {
 		if (tid % step == (step - 1) && (tid - (step / 2) >= bid)) {
 			array[tid] += array[tid - (step / 2)];
 		}
@@ -76,25 +48,31 @@ void upsweep (float * array, float * array_aggr1, int size, int size_aggr1) {
 	}
 	__syncthreads();
 
-	if (threadIdx.x == (TPB - 1)) {
-		if (tid < size) {
-			array_aggr1[blockIdx.x] = array[tid];
+	// Aggregates the sum of each block to another array for to calculate partial tums
+
+	if (array_aggr1 != NULL) {
+		if (threadIdx.x == (TPB - 1)) {
+			if (tid < size) {
+				array_aggr1[blockIdx.x] = array[tid];
+			}
+			else {
+				array_aggr1[blockIdx.x] = array[size - 1];
+			}
 		}
-		else {
-			array_aggr1[blockIdx.x] = array[size - 1];
-		}
+		__syncthreads();
 	}
-	__syncthreads();
 }
 
-
 __global__ 
-void downsweep (float * array) {
+void incl_downsweep (double * array) {
 	
 	int next_bid = (blockIdx.x + 1) * blockDim.x;
 	int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
 
 	__syncthreads();
+
+	// Performs an inclusive down sweep. After the inclusive down sweep, each block
+	// will have elements a_1, a_1 + a_2, ... , a_1 + a_2 + ... + a_1024
 
 	for (int step = TPB / 2; step > 1; step /= 2) {
 		if (tid % step == (step - 1) && (tid + (step / 2) < next_bid)) {
@@ -102,20 +80,33 @@ void downsweep (float * array) {
 		}
 		__syncthreads();
 	}
-	__syncthreads();
-	
-
-/*
-	if (tid >= (1<<step) && (tid < size)) {
-		tmp = array[tid - (1<<step)];
-		__syncthreads();
-
-		array[tid] = tmp + array[tid];
-		__syncthreads();
-	}
-*/
 }
 
+__global__
+void excl_downsweep (double * array, int size) {
+
+	int bsize = blockIdx.x * blockDim.x;
+	int next_block = (blockIdx.x + 1) * blockDim.x;
+	int tid = bsize + threadIdx.x;
+	int tmp;
+
+	int min_size = min(size, TPB);
+
+	// Performs an exlusive down sweep. After the inclusive down sweep, each block
+	// will have elements 0, 0 + a_1 , 0 + a_1 + a_2, ... , 0 + a_1 + a_2 + ... + a_1023
+
+	if (tid % TPB == 0) { array[min(size, next_block) - 1] = 0; } 
+	__syncthreads();
+
+	for (int step = min_size; step > 0; step /= 2) {
+		if (tid % step == (step - 1) && (tid - (step / 2) >= bsize)) {
+			tmp = array[tid];
+			array[tid] += array[tid - (step / 2)];
+			array[tid - (step / 2)] = tmp;
+		}
+		__syncthreads();
+	}
+}
 
 int main(int argc, char** argv)
 {
@@ -125,11 +116,11 @@ int main(int argc, char** argv)
 	}
 
 	int size = atoi(argv[1]);
-	int size_div1 = int(ceil(float(size) / float(TPB)));
-	int size_div2 = int(ceil(float(size_div1) / float(TPB)));
-	int nblocks = int(ceil(float(size) / float(TPB)));
-	int nblocks_div1 = int(ceil(float(nblocks) / float(TPB)));
-	int nblocks_div2 = int(ceil(float(nblocks_div1) / float(TPB)));
+	int size_div1 = int(ceil(double(size) / double(TPB)));
+	int size_div2 = int(ceil(double(size_div1) / double(TPB)));
+	int nblocks = int(ceil(double(size) / double(TPB)));
+	int nblocks_div1 = int(ceil(double(nblocks) / double(TPB)));
+	int nblocks_div2 = int(ceil(double(nblocks_div1) / double(TPB)));
 
 	cout << "First stage blocks: " << nblocks << endl;
 	cout << "Second stage blocks: " << nblocks_div1 << endl;
@@ -138,16 +129,18 @@ int main(int argc, char** argv)
 	cout << "Second stage size: " << size_div1 << endl;
 	cout << "Third stage size: " << size_div2 << endl;
 
+	cout << "min(size_div1, TPB): " << min(size_div1, TPB) << endl;
+
 	cout << "Malloc'ing\n";
-	float *x = (float*)malloc(size * sizeof(float));
-	float *x1 = (float*)malloc(size_div1 * sizeof(float));
-	float *x2 = (float*)malloc(size_div2 * sizeof(float));
-	float *y = (float*)malloc(size * sizeof(float));
+	double *x = (double*)malloc(size * sizeof(double));
+	double *x1 = (double*)malloc(size_div1 * sizeof(double));
+	double *x2 = (double*)malloc(size_div2 * sizeof(double));
+	double *y = (double*)malloc(size * sizeof(double));
 	
-	float *d_x, *d_x1, *d_x2;
-	cudaMalloc(&d_x, size * sizeof(float));
-	cudaMalloc(&d_x1, size_div1 * sizeof(float));
-	cudaMalloc(&d_x2, size_div2 * sizeof(float));
+	double *d_x, *d_x1, *d_x2;
+	cudaMalloc(&d_x, size * sizeof(double));
+	cudaMalloc(&d_x1, size_div1 * sizeof(double));
+	cudaMalloc(&d_x2, size_div2 * sizeof(double));
 
 	cout << "Generating Array\n";
 	srand(time(NULL));
@@ -156,82 +149,80 @@ int main(int argc, char** argv)
 		y[i] = x[i];
 	}
 
-	for (int i = 1; i < size; i++) {
-		y[i] = y[i] + y[i - 1];
-	}
-/*
-	for (int i = 1; i < size; i++) {
-		y[i] = y[i] + y[i - 1];
-	}
-*/
-
 	
-	cudaMemcpy(d_x, x, size * sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_x1, x1, size_div1 * sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_x2, x2, size_div2 * sizeof(float), cudaMemcpyHostToDevice);
+	clock_t seq_start, par_start;
+	clock_t seq_pass, par_pass;
+
+	cudaMemcpy(d_x, x, size * sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_x1, x1, size_div1 * sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_x2, x2, size_div2 * sizeof(double), cudaMemcpyHostToDevice);
 
 
-	cout << "Up-Sweep\n" << endl;
-	upsweep <<<nblocks, TPB>>> (d_x, d_x1, size, nblocks_div1);
+	for (int pf_step = 1; pf_step < 3; pf_step++) {
+	cout << "Prefix Sum, iteration " << pf_step << "\n\n";
+
+	cout << "Sequential Version" << endl;
+	seq_start = clock();
+	for (int i = 1; i < size; i++) {
+		y[i] = y[i] + y[i - 1];
+	}
+	seq_pass = clock() - seq_start;
+	cout << "Time taken: " << seq_pass * 1000 / CLOCKS_PER_SEC << endl;
+
+	cout << "Parallel Version" << endl;
+	par_start = clock();
+//	cout << "Up-Sweep\n" << endl;
+	upsweep <<<nblocks, TPB>>> (d_x, d_x1, size);
 	cudaDeviceSynchronize();
 
-	cout << "Down-Sweep\n" << endl;
-	downsweep <<<nblocks, TPB>>> (d_x);
+//	cout << "Up-Sweep 2\n" << endl;
+	upsweep <<<nblocks_div1, TPB>>> (d_x1, d_x2, size_div1);
+	cudaDeviceSynchronize();
+
+//	cout << "Up-Sweep 3\n" << endl;
+	upsweep <<<nblocks_div1, TPB>>> (d_x2, NULL, size_div2);
+	cudaDeviceSynchronize();
+
+//	cout << "Down-Sweep 3\n" << endl;
+	excl_downsweep <<<nblocks_div2, TPB>>> (d_x2, size_div2);
 	cudaDeviceSynchronize();
 
 /*
-	cout << "Up-Sweep 2\n" << endl;
-	upsweep <<<nblocks_div1, TPB>>> (d_x1, d_x2, size_div1, nblocks_div2);
-	cudaDeviceSynchronize();
-
-	cout << "Down-Sweep 2\n" << endl;
-	downsweep <<<nblocks_div1, TPB>>> (d_x1);
-	cudaDeviceSynchronize();
-*/
-
-	cout << "Inclusive Sum 1\n" << endl;
-	incl_pfsum <<<nblocks_div1, TPB>>> (d_x1, size_div1);
-	cudaDeviceSynchronize();
-
-	cudaMemcpy(x1, d_x1, size_div1 * sizeof(float), cudaMemcpyDeviceToHost);
-	cudaDeviceSynchronize();
-	for (int i = 0; i < size_div1; i++) {
-		cout << i << " " << x1[i] << endl;
-	}
-
-	cout << "Inclusive Sum 2\n" << endl;
-	incl_pfsum <<<nblocks_div2, TPB>>> (d_x2, size_div2);
-	cudaDeviceSynchronize();
-
-	cudaMemcpy(x2, d_x2, size_div2 * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(x2, d_x2, size_div2 * sizeof(double), cudaMemcpyDeviceToHost);
 	cudaDeviceSynchronize();
 	for (int i = 0; i < size_div2; i++) {
-		cout << i << " " << x2[i] << endl;
+		cout << i << " " << std::setprecision(10) << x2[i] << endl;
 	}
-
-	cout << "Scatter Partial Sums 2\n" << endl;
-	scat_part_sum <<<nblocks_div1, TPB>>> (d_x1, d_x2);
+*/
+//	cout << "Down-Sweep 2\n" << endl;
+	excl_downsweep <<<nblocks_div1, TPB>>> (d_x1, size_div1);
 	cudaDeviceSynchronize();
-
 /*
-	cudaMemcpy(x1, d_x1, size_div1 * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(x1, d_x1, size_div1 * sizeof(double), cudaMemcpyDeviceToHost);
 	cudaDeviceSynchronize();
 	for (int i = 0; i < size_div1; i++) {
-		cout << i << " " << x1[i] << endl;
+		cout << i << " " << std::setprecision(10) << x1[i] << endl;
 	}
 */
-
-	cout << "Scatter Partial Sums 1\n" << endl;
-	scat_part_sum <<<nblocks, TPB>>> (d_x, d_x1);
+//	cout << "Down-Sweep\n" << endl;
+	incl_downsweep <<<nblocks, TPB>>> (d_x);
 	cudaDeviceSynchronize();
 
-
+//	cout << "Scatter Partial Sums 2\n" << endl;
+	scat_part_sum <<<nblocks_div1, TPB>>> (d_x1, d_x2);
+	cudaDeviceSynchronize();
 /*
-	for (int i = 0; i < size; i++) {
-		cout << i << " " << x[i] << endl;
+	cudaMemcpy(x1, d_x1, size_div1 * sizeof(double), cudaMemcpyDeviceToHost);
+	cudaDeviceSynchronize();
+	for (int i = 0; i < size_div1; i++) {
+		cout << i << " " << std::setprecision(10) << x1[i] << endl;
 	}
 */
-
+//	cout << "Scatter Partial Sums 1\n" << endl;
+	scat_part_sum <<<nblocks, TPB>>> (d_x, d_x1);
+	cudaDeviceSynchronize();
+	par_pass = clock() - par_start;
+	cout << "Time taken: " << par_pass * 1000 / CLOCKS_PER_SEC << endl;
 
 /*	
 	thrust::inclusive_scan(x, x + size, x);
@@ -240,20 +231,26 @@ int main(int argc, char** argv)
 	cudaDeviceSynchronize();
 */
 
-	cudaMemcpy(x, d_x, size * sizeof(float), cudaMemcpyDeviceToHost);
+	cout << "Comparing Values" << endl;
+
+	cudaMemcpy(x, d_x, size * sizeof(double), cudaMemcpyDeviceToHost);
+	//cout << size-1 << " " << std::setprecision(10) << x[size-1] << " " << y[size-1] << endl;
 
 	for (int i = 0; i < size; i++) {
-		cout << i << " " << x[i] << " " << y[i] << endl;
+		//cout << i << " " << x[i] << " " << y[i] << endl;
 		if (x[i] != y[i]) {
-			//cout << i << " " << x[i] << " " << y[i] << endl;
-			//cout << "Not the same" << endl;
-			//return 0;
+			cout << "Not the same at index " << i << endl;
+			cout << i << " " << x[i] << " " << y[i] << endl;
+			if (pf_step == 1) { break; }
+			else { return 0;}
 
 		}
 	}
 
+	cout << "arrays are the same\n\n\n";
 
-	cout << "arrays are the same" << endl;
+	}
+
 
 
 	return 0;
