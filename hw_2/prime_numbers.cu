f587258dc012e49af2da37dff811e5276acb9e94
mingtaiha <ming.tai.ha@gmail.com>
2017-02-22 11:43:46 -0500

    need to fix bug in parallel implementation

--- hw_2/prime_numbers.cu
+++ hw_2/prime_numbers.cu
@@ -48,6 +48,30 @@ void seq_sieve(int * arr, int n) {
 	}
 }
 
+__global__
+void par_sieve(int * d_arr, int n, int sqrt_n) {
+
+	int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
+	__syncthreads();
+
+	for (int i = 2; i <= sqrt_n; i++) {
+		if (tid < sqrt_n) {	
+			if (d_arr[i-2] == 1) {
+				for (int j = 0; j < n; j+=sqrt_n) {
+					if (((j + tid + i) < n) && (((j + tid + i) % i) == 0)) {
+						d_arr[j + tid + i] = 0;
+					}
+					__syncthreads();
+				}
+				__syncthreads();
+			}
+			__syncthreads();
+		}
+		__syncthreads();
+	}
+
+}
+
 
 
 int main(int argc, char** argv) {
@@ -59,9 +83,28 @@ int main(int argc, char** argv) {
 	int n = atoi(argv[1]);
 
 	int * seq_array = make_array_2_to_n(n);
-	print_array(seq_array, n);
+	//print_array(seq_array, n);
 	seq_sieve(seq_array, n);
-	print_prime(seq_array, n);
+	//print_prime(seq_array, n);
+
+	int sqrt_n = int(ceil(sqrt(float(n))));
+	int * par_array = make_array_2_to_n(n);
+	int * d_par_array;
+
+	cudaMalloc((void**)&d_par_array, sizeof(int) * (n-1));
+	cudaMemcpy((void*)d_par_array, (void*)par_array, sizeof(int) * (n-1), cudaMemcpyHostToDevice);
+
+	int nblocks = sqrt_n;
+	int tpb = sqrt_n;
+	
+	cout << "parallel \n\n\n";
+
+	par_sieve<<<nblocks, tpb>>>(d_par_array, n, sqrt_n);
+	cudaDeviceSynchronize();
+	
+	cudaMemcpy((void*)par_array, (void*)d_par_array, sizeof(int) * (n-1), cudaMemcpyDeviceToHost);
+	print_prime(par_array, n);
+
 
 	return 0;
 
