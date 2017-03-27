c23e2e22ca629f33546a654af32ac8fdbabed3c0
mingtaiha <ming.tai.ha@gmail.com>
2017-03-27 02:44:52 -0400

    modified genresult.cuh

--- hw_3/code_sample/genresult.cuh
+++ hw_3/code_sample/genresult.cuh
@@ -8,4 +8,12 @@ int getGridSize(int blockSize, int n);
 
 /* Initializes a MatrixInfo to store the result vector.*/
 MatrixInfo * initMatrixResult(int N, int blockSize);
+
+
+typedef struct A_cont {
+    int r;
+    int c;
+    float v;
+} A_cont;
+
 #endif
