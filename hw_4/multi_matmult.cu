76475d5e75b0d41f57a5ef7f4af96944b342f788
mingtaiha <ming.tai.ha@gmail.com>
2017-04-08 14:04:10 -0400

    Memcpy to Device done. Need to work on matrix multiplication

--- hw_4/multi_matmult.cu
+++ hw_4/multi_matmult.cu
@@ -19,23 +19,16 @@ int * def_mat_dim(int k)
     return dim;
 }
 
-double ** creat_mat(int dimX, int dimY)
+double * creat_mat(int dimX, int dimY)
 {
-    int x, y;
-    double ** mat = (double **) malloc(dimX * sizeof(double *));
-
-    int i;
-    for (i = 0; i < dimX; i++) {
-        mat[i] = (double *) malloc(dimY * sizeof(double));
-    }
+    int x;
+    double * mat = (double *) malloc(dimX * dimY * sizeof(double));
 
     srand(time(NULL));
 
-    for (x = 0; x < dimX; x++) {
-        for (y = 0; y < dimY; y++) {
-            mat[x][y] = float(rand()) / float(RAND_MAX) * SCALING_FACTOR;
-            //printf("%f\n", mat[x][y]);
-        }
+    for (x = 0; x < dimX * dimY; x++) {
+        mat[x] = float(rand()) / float(RAND_MAX) * SCALING_FACTOR;
+       //printf("%f\n", mat[x]);
     }
     return mat;
 }
@@ -48,7 +41,7 @@ void matmult(/* parameters */) {
 
 
 __global__
-void multi_matmult(/* parameters */)
+void multi_matmult(int num_dim, int * dim_list, double ** mat_list)
 {
 
 
@@ -59,16 +52,29 @@ void multi_matmult(/* parameters */)
 int main()
 {
 
-    int num_dim = 10;
+    int num_dim = 2;
     int * mat_dim = def_mat_dim(num_dim);
-    double *** mat_list = (double ***) malloc((num_dim-1) * sizeof(double **));
+    double ** mat_list = (double **) malloc((num_dim-1) * sizeof(double *));
 
     int k;
     for (k = 0; k < num_dim-1; k++) {
-        printf("================= MATRIX %d ====================\n", k);
+        //printf("================= MATRIX %d ====================\n", k);
         mat_list[k] = creat_mat(mat_dim[k], mat_dim[k+1]);
     }
 
+    double * out_mat;
+
+    int * d_mat_dim;
+    cudaMalloc(&d_mat_dim, num_dim * sizeof(int));
+    cudaMemcpy(d_mat_dim, mat_dim, num_dim * sizeof(int), cudaMemcpyHostToDevice);
+
+    double ** d_mat_list;
+    cudaMalloc(&d_mat_list, num_dim-1 * sizeof(double *));
+
+    for (k = 0; k < num_dim - 1; k++) {
+        cudaMalloc(&d_mat_list[k], mat_dim[k] * mat_dim[k+1] * sizeof(double));
+        cudaMemcpy(d_mat_list[k], mat_list[k], num_dim-1 * sizeof(double), cudaMemcpyHostToDevice);
+    }
 
 
 
