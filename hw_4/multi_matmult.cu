6de6b8fc8b82e44f0fc3d8d14a1a258094f20099
mingtaiha <ming.tai.ha@gmail.com>
2017-04-17 03:06:06 -0400

    fixed host to device memory transfer, need to verify

--- hw_4/multi_matmult.cu
+++ hw_4/multi_matmult.cu
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_DIM 1024
+#define MAX_DIM 32
 #define SCALING_FACTOR 100.0
-
+#define TILE_DIM 32
+#define NUM_THREADS 1024
 
 int * def_mat_dim(int k)
 {
@@ -14,7 +15,8 @@ int * def_mat_dim(int k)
 
     for (i = 0; i < k; i++)
     {
-        dim[i] = rand() % MAX_DIM + 1;
+        dim[i] = (rand() % MAX_DIM) + 1;
+        printf("%d\n", dim[i]);
     }
     return dim;
 }
@@ -44,6 +46,45 @@ __global__
 void multi_matmult(int num_dim, int * dim_list, double ** mat_list)
 {
 
+}
+
+
+__global__ void MatMul(double* A, double* B, double* C, int ARows, int ACols, int BRows,
+    int BCols, int CRows, int CCols)
+{
+    float CValue = 0;
+
+    int Row = blockIdx.y*TILE_DIM + threadIdx.y;
+    int Col = blockIdx.x*TILE_DIM + threadIdx.x;
+
+    __shared__ double As[TILE_DIM][TILE_DIM];
+    __shared__ double Bs[TILE_DIM][TILE_DIM];
+
+    for (int k = 0; k < (TILE_DIM + ACols - 1)/TILE_DIM; k++) {
+
+         if (k*TILE_DIM + threadIdx.x < ACols && Row < ARows)
+             As[threadIdx.y][threadIdx.x] = A[Row*ACols + k*TILE_DIM + threadIdx.x];
+         else
+             As[threadIdx.y][threadIdx.x] = 0.0;
+
+         if (k*TILE_DIM + threadIdx.y < BRows && Col < BCols)
+             Bs[threadIdx.y][threadIdx.x] = B[(k*TILE_DIM + threadIdx.y)*BCols + Col];
+         else
+             Bs[threadIdx.y][threadIdx.x] = 0.0;
+
+         __syncthreads();
+
+         for (int n = 0; n < TILE_DIM; ++n)
+             CValue += As[threadIdx.y][n] * Bs[n][threadIdx.x];
+
+         __syncthreads();
+    }
+
+    if (Row < CRows && Col < CCols)
+        C[((blockIdx.y * blockDim.y + threadIdx.y)*CCols) +
+           (blockIdx.x * blockDim.x)+ threadIdx.x] = CValue;
+
+
 
 }
 
@@ -52,34 +93,82 @@ void multi_matmult(int num_dim, int * dim_list, double ** mat_list)
 int main()
 {
 
-    int num_dim = 2;
+    int num_dim = 3;
+    int num_mat = num_dim - 1;
     int * mat_dim = def_mat_dim(num_dim);
-    double ** mat_list = (double **) malloc((num_dim-1) * sizeof(double *));
+    double ** mat_list = (double **) malloc((num_mat) * sizeof(double *));
 
-    int k;
-    for (k = 0; k < num_dim-1; k++) {
-        //printf("================= MATRIX %d ====================\n", k);
-        mat_list[k] = creat_mat(mat_dim[k], mat_dim[k+1]);
-    }
 
-    double * out_mat;
+//    printf("Copying matrix dimensions to device\n");
 
     int * d_mat_dim;
-    cudaMalloc(&d_mat_dim, num_dim * sizeof(int));
+    cudaMalloc((void **)&d_mat_dim, num_dim * sizeof(int));
     cudaMemcpy(d_mat_dim, mat_dim, num_dim * sizeof(int), cudaMemcpyHostToDevice);
 
-    double ** d_mat_list;
-    cudaMalloc(&d_mat_list, num_dim-1 * sizeof(double *));
+//    printf("Creating Matrix from on host\n");
 
-    for (k = 0; k < num_dim - 1; k++) {
-        cudaMalloc(&d_mat_list[k], mat_dim[k] * mat_dim[k+1] * sizeof(double));
-        cudaMemcpy(d_mat_list[k], mat_list[k], num_dim-1 * sizeof(double), cudaMemcpyHostToDevice);
+    int k;
+    for (k = 0; k < num_mat; k++) {
+        //printf("================= MATRIX %d ====================\n", k);
+        printf("%d %d\n", mat_dim[k], mat_dim[k+1]);
+        mat_list[k] = creat_mat(mat_dim[k], mat_dim[k+1]);
+    }
+    
+//    printf("Allocating space to store output matrix\n");
+    double * out_mat = (double *) malloc(mat_dim[0] * mat_dim[num_dim-1] * sizeof(double));
+    double * d_out_mat;
+    cudaMalloc((void **) &d_out_mat, mat_dim[0] * mat_dim[num_dim-1] * sizeof(double));
+
+//    printf("Allocating space for each matrix, and storing pointer address of matrices on the host\n");
+    double ** int_mat_list = (double **) malloc(num_mat * sizeof(double *));
+    for (k = 0; k < num_mat; k++) {
+        cudaMalloc((void **)&int_mat_list[k], mat_dim[k] * mat_dim[k+1] * sizeof(double));
+        cudaMemcpy(int_mat_list[k], mat_list[k], mat_dim[k] * mat_dim[k+1] * sizeof(double), cudaMemcpyHostToDevice);
     }
 
+//    printf("Copying pointer addresses of matrices from host to device\n");
+    double ** d_mat_list;
+    cudaMalloc(&d_mat_list, num_mat * sizeof(double *));
+    cudaMemcpy(d_mat_list, int_mat_list, num_mat * sizeof(double *), cudaMemcpyHostToDevice);
 
 
+    cudaError_t error = cudaGetLastError();
+    if(error != cudaSuccess)
+    {
+       // print the CUDA error message and exit
+       printf("CUDA error: %s\n", cudaGetErrorString(error));
+       exit(-1);
+    }
+
+/*
+    for (k = 0; k < num_dim-1; k++) {
+        printf("%d %d %d %d\n", k, mat_dim[k], mat_dim[k+1], &d_mat_list[k]);
+        cudaMalloc((void **)&d_mat_list[k], mat_dim[k] * mat_dim[k+1] * sizeof(double));
+        //cudaMemcpy(d_mat_list[k], mat_list[k], mat_dim[k] * mat_dim[k+1] * sizeof(double), cudaMemcpyHostToDevice);
+
+        if(error != cudaSuccess)
+        {
+           // print the CUDA error message and exit
+           printf("CUDA error: %s\n", cudaGetErrorString(error));
+           exit(-1);
+        }
+    }
+    printf("After d_mat_list\n");
+*/
+
+//    printf("At the kernel call\n");
+    printf("%d %d %d\n", mat_dim[0], mat_dim[1], mat_dim[2]);
+    MatMul<<<2, NUM_THREADS>>>(int_mat_list[0], int_mat_list[1], d_out_mat, mat_dim[0], mat_dim[1], mat_dim[1], mat_dim[2], mat_dim[0], mat_dim[2]);
+    cudaThreadSynchronize();
 
 
+    error = cudaGetLastError();
+    if(error != cudaSuccess)
+    {
+       // print the CUDA error message and exit
+       printf("CUDA error: %s\n", cudaGetErrorString(error));
+       exit(-1);
+    }
 
     return 0;
 
